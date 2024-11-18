7ca42fddb3eb1acea1752ce9741ef44f62f17a99
David Rey <neisserrey@upeu.edu.pe>
2024-11-18 09:22:43 -0500

    fix: :boom: Cambiamos el path a admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('')
             ->databaseTransactions()
             ->login(Login::class)
             ->globalSearch(false)
